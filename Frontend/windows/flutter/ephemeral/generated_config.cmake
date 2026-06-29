7faaf658c76363541a16aee2477ef844c7b9384a
Syed Muhammad Ali Hassan <alihassa1226@gmail.com>
2026-06-29 13:35:38 +0500

    Add files via upload

---
+++ Frontend/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter_windows_3.24.2-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\CWS-main\\Frontend" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter_windows_3.24.2-stable\\flutter"
+  "PROJECT_DIR=D:\\CWS-main\\Frontend"
+  "FLUTTER_ROOT=C:\\flutter_windows_3.24.2-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\CWS-main\\Frontend\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\CWS-main\\Frontend"
+  "FLUTTER_TARGET=D:\\CWS-main\\Frontend\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDEuMC0wLjIucHJl,RkxVVFRFUl9DSEFOTkVMPWJldGE=,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049YTdjNGM2YmY3NA==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MTVkMTM3NjQ3ZQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMS4wIChidWlsZCAzLjExLjAtMjk2LjQuYmV0YSk="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\CWS-main\\Frontend\\.dart_tool\\package_config.json"
+)
